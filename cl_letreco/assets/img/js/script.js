3e28c14f2b823ee2e0d134797f97fbe2c1614ad3
David Freitas <davidsantosfreitas13@gmail.com>
2022-07-01 23:19:48 -0300

    Mecanica de inserção de textos e passar de linha feita

--- cl_letreco/assets/img/js/script.js
+++ cl_letreco/assets/img/js/script.js
@@ -1,7 +1,49 @@
-let table = {
-   
-    letra_pos: 0,
-    linha_pos: 0
-    
-}
+let control = {
+  letra_pos: "0",
+  linha_pos: "0",
+  id: "00",
 
+  nextLetter() {
+    if (this.letra_pos < 5) {
+      this.letra_pos = String(Number(this.letra_pos) + 1);
+    }
+  },
+  backLetter() {
+    if (this.letra_pos > 0) {
+      this.letra_pos = String(Number(this.letra_pos) - 1);
+    }
+  },
+
+  nextRow() {
+
+    if(Number(this.letra_pos) > 3){
+    this.linha_pos = String(Number(this.linha_pos) + 1);
+
+    this.letra_pos = "0"
+    this.actId()  
+}
+  },
+
+  actId() {
+    this.id = this.linha_pos + this.letra_pos;
+  },
+// \\\\\\\\\\\\\\\
+
+  insert(letter) {
+    if ( this.letra_pos < 5) {
+
+        document.getElementById(this.id).innerHTML = letter;
+        this.nextLetter();
+        this.actId();
+    } 
+  },
+
+  delete() {
+    this.backLetter();
+    this.actId();
+    document.getElementById(this.id).innerHTML = "";
+ 
+  },
+
+
+}
